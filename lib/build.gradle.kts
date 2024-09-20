9ad706edacd8971c909bccf07c7604fe1a1b9761
meres <meres@meres>
2024-09-20 09:43:35 +0200

    finish lab

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,15 +1,7 @@
 plugins {
-    java
-    jacoco
+    id("hu.bme.mit.ase.shingler.gradle.java")
 }
 
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-repositories {
-    mavenCentral()
-}
 
 val picoCliVersion = "4.7.6"
 val slf4jVersion = "1.7.36"
@@ -25,16 +17,4 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-}
-
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
-
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
 }
